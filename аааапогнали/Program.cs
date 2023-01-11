0c6064c7b46ea6b0d73c30669f53de715ec4705a
Patron <Patron@LAPTOP-VIEAF2J0>
2023-01-12 02:46:33 +0300

    Добавьте файлы проекта.

---
+++ аааапогнали/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace аааапогнали
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            for (int i = 1000; i > 2; i -= 3)
+            {
+                Console.WriteLine(i + " - 3 = " + (i - 3));
+                System.Threading.Thread.Sleep(200);
+             
+                
+          
+
+
+            }
+
+
+
+
+
+        }
+    }
+}
